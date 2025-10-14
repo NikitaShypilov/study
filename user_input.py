05441af4288c226b23f7c7f26ea1cb7bdc9088ec
NikitaShypilov <holdikkot@gmail.com>
2025-10-14 19:52:58 +0300

    2.1 hw

---
+++ user_input.py
@@ -0,0 +1,14 @@
+user_input1 = input("enter 1 number:")
+user_input2 = input("enter 2 number:")
+user_input3 = input("enter 3 number:")
+user_input4 = input("enter 4 number:")
+
+number1 = int(user_input1)
+number2 = int(user_input2)
+number3 = int(user_input3)
+number4 = int(user_input4)
+
+print(number1)
+print(number2)
+print(number3)
+print(number4)
