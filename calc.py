78d7a46e854638e5bd3c1cc24d01826d2189eaa5
NikitaShypilov <holdikkot@gmail.com>
2025-10-21 22:00:37 +0300

    5.2 hw

--- calc.py
+++ calc.py
@@ -1,22 +1,26 @@
-n1 = int(input("Enter the first number: "))
-n2 = int(input("Enter the second number: "))
+while True:
 
+    n1 = int(input("Enter the first number: "))
+    n2 = int(input("Enter the second number: "))
 
-action = input("Enter the action + or - or * or /:")
+    action = input("Enter the action + or - or * or /:")
 
-if action == "/" and n2 == 0:
-    print('Cant solve')
+    if action == "+":
+        print(f"Result: {n1} + {n2} = {n1 + n2}")
 
-elif action == "+":
-    print(f"Result: {n1} + {n2} = {n1 + n2}")
+    elif action == "-":
+        print(f"Result: {n1} - {n2} = {n1 - n2}")
 
-elif action == "-":
-    print(f"Result: {n1} - {n2} = {n1 - n2}")
+    elif action == "*":
+        print(f"Result: {n1} * {n2} = {n1 * n2}")
 
-elif action == "*":
-    print(f"Result: {n1} * {n2} = {n1 * n2}")
+    elif action == "/":
+        if action == "/" and n2 == 0:
+            print('Cant solve')
+        else:
+            print(f"Result: {n1} / {n2} = {n1 / n2}")
 
-elif action == "/":
-    print(f"Result: {n1} / {n2} = {n1 / n2}")
-
-
+    cont = input("Want to continue? (y/n): ")
+    if cont != "y":
+        print("Bye!")
+        break
