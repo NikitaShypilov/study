8fe9f9fed9dc5d9eb7a3cf01e87a88a9d73034d4
NikitaShypilov <holdikkot@gmail.com>
2025-10-27 13:13:10 +0200

    6.2 hw rework

--- time.py
+++ time.py
@@ -11,7 +11,20 @@ rem = rem % 3600
 minutes = rem // 60
 seconds = rem % 60
 
+hours = str(hours).zfill(2)
+minutes = str(minutes).zfill(2)
+seconds = str(seconds).zfill(2)
+
+if days % 10 == 1 and days % 100 != 11:
+    day_word = "день"
+elif 2 <= days % 10 <= 4 and not (12 <= days % 100 <= 14):
+    day_word = "дні"
+else:
+    day_word = "днів"
+
+
 if time_input < 0 or time_input > 8640000:
     print("Invalid number")
 else:
-    print(f"Days:{days}, hours {hours}, minutes {minutes}, seconds {seconds}")
+    print(f"{days} {day_word}, {hours}:{minutes}:{seconds}")
+
