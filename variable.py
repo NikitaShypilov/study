ae03318a6eb2bb28cb7cb138cd1c5232fc3b62d5
NikitaShypilov <holdikkot@gmail.com>
2025-10-27 08:46:17 +0300

    5.1 hw

---
+++ variable.py
@@ -0,0 +1,25 @@
+import string
+import keyword
+
+name = input("Input name of variable: ")
+
+if name in keyword.kwlist:
+    print(False)
+
+elif name and name[0].isdigit():
+    print(False)
+
+elif any(ch.isupper() for ch in name):
+    print(False)
+
+elif any(ch in string.punctuation.replace("_", "") or ch.isspace() for ch in name):
+    print(False)
+
+elif name.count("_") > 1:
+    print(False)
+
+elif not name:
+    print(False)
+
+else:
+    print(True)
