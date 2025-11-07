ad579081f8392e8cae31e44013258da264c3d9cc
Nikita Shypilov <holdikkot@gmail.com>
2025-11-07 13:29:17 +0300

    Update add.py

--- add.py
+++ add.py
@@ -1,8 +1,9 @@
-def add_one(some_list):
+def add_one(some_list: list[int]):
     num = int(''.join(str(d) for d in some_list))
     num += 1
     result = [int(ch) for ch in str(num)]
     return result
+    
 assert add_one([1, 2, 3, 4]) == [1, 2, 3, 5], 'Test1'
 assert add_one([9, 9, 9]) == [1, 0, 0, 0], 'Test2'
 assert add_one([0]) == [1], 'Test3'
