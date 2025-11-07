1e188baf725d06ce645a40e67c8870ea3366969e
Nikita Shypilov <holdikkot@gmail.com>
2025-11-07 13:34:39 +0300

    Update difference.py

--- difference.py
+++ difference.py
@@ -5,6 +5,7 @@ def difference(*args):
     min_val = min(args)
     diff = max_val - min_val
     return round(diff, 2)
+
 assert difference(1, 2, 3) == 2, 'Test1'
 assert difference(5, -5) == 10, 'Test2'
 assert difference(10.2, -2.2, 0, 1.1, 0.5) == 12.4, 'Test3'
